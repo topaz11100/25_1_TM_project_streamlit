9f058cbf1fe63a52baf0cd5b77e287488c68d7d9
topaz <topaz11100@gmail.com>
2025-06-12 23:48:25 +0900

    1

--- app.py
+++ app.py
@@ -1,1 +1,52 @@
-#1
+import streamlit as st
+from process import load_data, query_process, work_process, make_lecture_query, lecture_process, create_prompt, make_output, WORK_K, LECTURE_K, WORK_COUNT, LECTURE_COUNT
+
+model, work_name, work_info, lecture_name, work_idx, f_idx, work_f_idx, work_f_name, lecture_idx, client = load_data()
+
+with open("intro.md", "r", encoding="utf-8") as f:
+    intro_md = f.read()
+
+with st.container():
+
+    user_input = st.chat_input('무엇을 하고 싶으신가요?')
+
+    if user_input:
+        user = st.chat_message("user")
+        
+        user.write(user_input)
+
+        with st.status("처리 중...", expanded=True) as status:
+            st.write("사용자 입력 임베딩 중...")
+            work_query = query_process([user_input], model)
+            
+            st.write("직업과의 유사도 계산 중...")
+            work_name_out, work_sim = work_process(work_query, WORK_K, WORK_COUNT, work_idx, work_name, f_idx, work_f_idx, work_f_name)
+        
+            st.write("직업을 기반으로 강의와의 유사도 계산 중...")
+            lecture_query = make_lecture_query(work_name_out, work_info, model)
+            lecture_name_out, lecture_sim = lecture_process(lecture_query, LECTURE_K, LECTURE_COUNT, lecture_idx, lecture_name)
+            
+            st.write("답변 생성 중...")
+            prompt = create_prompt(user_input, work_name_out, work_sim, lecture_name_out, lecture_sim)
+            gpt_out = make_output(prompt, client)
+
+            status.update(label="✅ 처리 완료!", state="complete", expanded=False)
+        
+        detail_result = (
+            f'출력\n{gpt_out}\n'
+            f'work_name : {work_name_out}\n'
+            f'work_sim : {work_sim}\n'
+            f'lecture_name : {lecture_name_out}\n'
+            f'lecture_sim : {lecture_sim}\n'
+        )
+
+        ai = st.chat_message("ai")
+        ai.markdown(gpt_out)
+
+intro, detail = st.tabs(["프로젝트 소개", "상세 결과"])
+
+with intro:
+    st.markdown(intro_md)
+with detail:
+    st.write(detail_result)
+
